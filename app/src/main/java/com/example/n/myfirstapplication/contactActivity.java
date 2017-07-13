26ebe91262223b6a8a51ae989ca3fbb62cb1aacd
Vincent Nio <vnio666@aucklanduni.ac.nz>
2017-07-13 14:57:26 +1200

    Contacts alphabetically ordered

--- app/src/main/java/com/example/n/myfirstapplication/contactActivity.java
+++ app/src/main/java/com/example/n/myfirstapplication/contactActivity.java
@@ -16,6 +16,7 @@ import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashSet;
 
 public class contactActivity extends AppCompatActivity {
@@ -47,7 +48,10 @@ public class contactActivity extends AppCompatActivity {
                     contactList.add(ds.getValue().toString());
                 }
                 adapter.clear();
-                adapter.addAll(new ArrayList<>(contactList));
+
+                ArrayList<String> listViewData = new ArrayList<>(contactList);
+                Collections.sort(listViewData);
+                adapter.addAll(listViewData);
                 adapter.notifyDataSetChanged();
             }
 
