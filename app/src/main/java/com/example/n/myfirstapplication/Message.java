b9f6d3f8ab8270834503b5612763d6087c61b74e
John Pham <dpha010@aucklanduni.ac.nz>
2017-07-14 15:02:39 +1200

    add search item menu and imageurl field to message class

--- app/src/main/java/com/example/n/myfirstapplication/Message.java
+++ app/src/main/java/com/example/n/myfirstapplication/Message.java
@@ -11,15 +11,57 @@ class Message {
 
     public String time;
     public String message;
+    public String imageUrl;
     public String userTo; //user which receives
     public String userFrom; //user which sent
 
     public Message(){}
 
-    public Message(String time, String message, String userTo, String userFrom){
+    public Message(String time, String message, String imageUrl, String userTo, String userFrom){
         this.time = time;
         this.message = message;
+        this.imageUrl= imageUrl;
         this.userTo = userTo;
+        this.userFrom = userFrom;
+    }
+
+    public String getTime() {
+        return time;
+    }
+
+    public void setTime(String time) {
+        this.time = time;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+
+    public String getUserTo() {
+        return userTo;
+    }
+
+    public void setUserTo(String userTo) {
+        this.userTo = userTo;
+    }
+
+    public String getUserFrom() {
+        return userFrom;
+    }
+
+    public void setUserFrom(String userFrom) {
         this.userFrom = userFrom;
     }
 }
